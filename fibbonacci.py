0e29d4b17754d0181f440a42d3236306bccfe86d
HafseeMan <58669207+HafseeMan@users.noreply.github.com>
2020-08-13 16:12:23 +0100

    first commit

---
+++ fibbonacci.py
@@ -0,0 +1,23 @@
+
+#Python function to output the Fibbonacci sequence of a given number
+def fibbonacci(given_num):
+    a = 0
+    b = 1
+    sum = 0
+    count = 1
+    while(count <= given_num):
+        print(sum, end = " ")
+        count += 1
+        a = b
+        b = sum
+        sum = a + b
+
+
+
+#Prompt user for number
+n = int(input("Enter the value of n = "))
+
+#Output format
+print("Fibbonacci Series: ", end = " ")
+fibbonacci(n)
+
